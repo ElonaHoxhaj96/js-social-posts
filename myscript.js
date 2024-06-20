c7ad6e0006d63a404545a77145446c6971230566
ElonaHoxhaj96 <elona6915@gmail.com>
2024-06-20 16:56:19 +0200

    feat: create container in page by using js

--- myscript.js
+++ myscript.js
@@ -1,4 +1,10 @@
-//creo un ciclo che mi stampi che mi stampi gli elementi della array opject
+//creo un ciclo che mi stampi che mi stampi gli elementi della array opject in console
 posts.forEach(element => {
     console.log(element);
 });
+// creo i contenitori in pagina e stampo i miei elementi 
+const divPost = document.createElement('div');
+divPost.classList.add('post');
+
+let container = document.getElementById('container');
+container.appendChild(divPost);

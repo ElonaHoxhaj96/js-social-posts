98c7a168331942210801d19e2865d8ebf497915b
ElonaHoxhaj96 <elona6915@gmail.com>
2024-06-20 18:03:20 +0200

    Fix: printing object in page

--- myscript.js
+++ myscript.js
@@ -1,6 +1,7 @@
 //creo un ciclo che mi stampi che mi stampi gli elementi della array opject in console
 posts.forEach(element => {
-    
+    // const { id, content,media, author, likes, created}= posts;
+    console.log(element.author.name);
     // creo i contenitori in pagina e stampo i miei elementi 
     const divPost = document.createElement('div');
     divPost.classList.add('post');
@@ -8,23 +9,21 @@ posts.forEach(element => {
     let container = document.getElementById('container');
     container.appendChild(divPost);
    //creo una funzione per stampare in pagina i vari elementi del mio array object
-   
-   function creaPost (){
     let contenutoPost = `
             <div class="post__header">
                 <div class="post-meta">                    
                     <div class="post-meta__icon">
-                        <img class="profile-pic" src=${image} alt="Phil Mangione">                    
+                        <img class="profile-pic" src=${element.author.image} alt="Phil Mangione">                    
                     </div>
                     <div class="post-meta__data">
-                        <div class="post-meta__author">${image1}</div>
-                        <div class="post-meta__time">${created}</div>
+                        <div class="post-meta__author">${element.author.name}</div>
+                        <div class="post-meta__time">${element.created}</div>
                     </div>                    
                 </div>
             </div>
-            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+            <div class="post__text">${element.content}</div>
             <div class="post__image">
-                <img src="https://unsplash.it/600/300?image=171" alt="">
+                <img src=${element.media} alt="">
             </div>
             <div class="post__footer">
                 <div class="likes js-likes">
@@ -35,10 +34,10 @@ posts.forEach(element => {
                         </a>
                     </div>
                     <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                        Piace a <b id="${element.id}" class="js-likes-counter">80</b> persone
                     </div>
                 </div> 
             </div>`
-   };
-   divPost.innerHTML = creaPost;
+
+   divPost.innerHTML = contenutoPost;
 });
